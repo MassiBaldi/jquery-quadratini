2f5134aa307328a83f6b82801331ba9922c2faa0
MassiBaldi <massibaldi85@gmail.com>
2018-11-28 23:52:58 +0100

    Fine

--- main.js
+++ main.js
@@ -5,9 +5,19 @@
 // Sopra alla griglia deve esserci un contatore che
 // conta quanti rossi e quanti verdi sono stati scoperti
 
-$('.contenitore > div').click(function(verde){
-  $(this).addClass('green')
-});
-$('.wrong').click(function(rossa){
+
+var count1 = 0;
+
+$('.rossi').click(function() {
   $(this).addClass('red')
+  count1++
+  $('#counter1').html('Quadrati Rossi: ' + count1)
+});
+
+var count2 = 0;
+
+$('.verdi').click(function(){
+  $(this).addClass('green')
+  count2++
+  $('#counter2').html('Quadrati Verdi: ' + count2)
 });
